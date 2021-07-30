dd5dfb9e7fe2f39fb156d382944e47aca388b190
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-07-30 21:47:10 +0600

    about section almost done except image

---
+++ src/components/About/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './About.css';
+// import aboutImg from '../../../images/';
+import aboutImage from '../../images/standpic.jpg'
+
+const About = () => {
+    return (
+        <section className='about_section' id='about'>
+            <div className='about_img'>
+                <div className='img_div'>
+                    <img src={aboutImage} alt="" />
+                </div>
+            </div>
+
+            <div className='about_text'>
+                <div className='text_div'>
+                    <h4>About</h4>
+                    <p>
+                        I am a curious front end developer who is looking for a internship in
+                        order to enhance my skills and gain more experience and knowledge. I am studying
+                        in Computer Science and Engineering.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
--- src/components/Home/MyImage/MyImage.js
+++ src/components/Home/MyImage/MyImage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MyImage.css';
-import proPic from '../../../images/propic2.png'
+import proPic from '../../../images/propic3.png'
+// import proPic from '../../../images/'
 
 const MyImage = () => {
     return (
