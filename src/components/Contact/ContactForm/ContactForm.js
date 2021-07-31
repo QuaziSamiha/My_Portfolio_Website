17370129b733fded687796bef63d65db6090c317
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-07-31 22:53:10 +0600

    formspree is used for contact section

--- src/components/Contact/ContactForm/ContactForm.js
+++ src/components/Contact/ContactForm/ContactForm.js
@@ -6,7 +6,7 @@ const ContactForm = () => {
         <div className='message_div'>
             <h4>Send Message</h4>
             <div className='form_div'>
-                <form action="" method="post">
+                <form action="https://formspree.io/f/xdoywgqo" method="post">
                     <input type="text" id='name' placeholder='Your Name' />
                     <input type="email" name="email" id="senderEmail" placeholder='Your Email' />
                     <textarea name="msg" id="" cols="30" rows="10" placeholder='write message...'></textarea>
